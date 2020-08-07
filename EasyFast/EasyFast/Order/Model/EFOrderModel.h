e31a2015c38e3c9b8dac4200d7df433b76093c22
easyfast <876231865@qq.com>
2020-08-07 16:09:59 +0800

    加入订单模型

---
+++ EasyFast/EasyFast/Order/Model/EFOrderModel.h
@@ -0,0 +1,17 @@
+//
+//  EFOrderModel.h
+//  EasyFast
+//
+//  Created by easyfast on 2020/8/7.
+//  Copyright © 2020 EasyFast. All rights reserved.
+//
+
+#import "EFBaseModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EFOrderModel : EFBaseModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ EasyFast/EasyFast/Order/Model/EFOrderModel.m
@@ -0,0 +1,13 @@
+//
+//  EFOrderModel.m
+//  EasyFast
+//
+//  Created by easyfast on 2020/8/7.
+//  Copyright © 2020 EasyFast. All rights reserved.
+//
+
+#import "EFOrderModel.h"
+
+@implementation EFOrderModel
+
+@end
